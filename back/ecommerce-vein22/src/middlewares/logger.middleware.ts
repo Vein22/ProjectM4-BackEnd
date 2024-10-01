a6aeee8ab38f2653e2f31d002fea5cf3501e9c58
MMelvinMora <121642949+Vein22@users.noreply.github.com>
2024-09-30 23:50:58 -0400

    method update

--- back/ecommerce-vein22/src/middlewares/logger.middleware.ts
+++ back/ecommerce-vein22/src/middlewares/logger.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export function loggerGlobal(req: Request, res: Response, next: NextFunction) {
-    const currentDateTime = new Date().toISOString();
+    const currentDateTime = new Date().toDateString();
     console.log(`[${currentDateTime}] Estas ejecutando un método ${req.method} en la ruta ${req.url}`,);
     next();
 }
