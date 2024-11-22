3f3e79284e057a622b082b0f7dacfeda07031c60
MMelvinMora <121642949+Vein22@users.noreply.github.com>
2024-11-22 01:05:43 -0400

    22-11 Dependency injection

--- back/ecommerce-vein22/src/categories/category.module.ts
+++ back/ecommerce-vein22/src/categories/category.module.ts
@@ -9,5 +9,6 @@ import { Category } from './entities/category.entity';
   imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
   providers: [CategoriesService, CategoryRepository],
+  exports: [CategoryRepository],
 })
 export class CategoriesModule {}
--- back/ecommerce-vein22/src/products/products.module.ts
+++ back/ecommerce-vein22/src/products/products.module.ts
@@ -4,10 +4,13 @@ import { ProductsRepository } from "./products.repository";
 import { ProductsService } from "./products.service";
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { Product } from "./entities/product.entity";
+import { CategoryRepository } from "src/categories/category.repository";
+import { Category } from "src/categories/entities/category.entity";
+import { CategoriesModule } from "src/categories/category.module";
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Product])],
+    imports: [TypeOrmModule.forFeature([Product]), CategoriesModule],
     providers: [ProductsService, ProductsRepository],
     controllers: [ProductsController],
     exports: [ProductsService, ProductsRepository],
